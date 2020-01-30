a87aaaf5b568088905f2fc2a4631b1d2f870569b
Юрий Кузьминых <yurykuz65@gmail.com>
2020-01-30 14:28:30 +0300

    Сделал алгоритм выдачи объектам уникальных имён

---
+++ Assets/Scripts/Editor/UniqueNamer.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public static class UniqueNamer
+{
+    [MenuItem("Geekbrains/Дать объектам уникальные имена")]
+    static void GiveUniqueNames()
+    {
+        Dictionary<string, int> namesCount = new Dictionary<string, int>();
+        var allGameObjects = Object.FindObjectsOfType<GameObject>();
+        foreach (var gameObject in allGameObjects)
+        {
+            if (namesCount.ContainsKey(gameObject.name))
+            {
+                namesCount[gameObject.name]++;
+            }
+            else
+            {
+                namesCount.Add(gameObject.name, 1);
+            }
+        }
+
+        foreach (var pair in namesCount)
+        {
+            if (pair.Value > 1)
+            {
+                var gameObjects = allGameObjects.Where(x => x.name == pair.Key);
+                int number = 1;
+                foreach (var gameObject in gameObjects)
+                {
+                    gameObject.name += $"({number})";
+                    number++;
+                }
+            }
+        }
+
+        EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
+    }
+}
